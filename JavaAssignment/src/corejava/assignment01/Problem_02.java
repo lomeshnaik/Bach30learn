ede86dd30dff03afeaa09993f0de5fb2f33e4642
jayramrout <jayram.h2kinfosys@gmail.com>
2019-06-12 17:40:33 -0400

    Its a division Problem

--- JavaAssignment/src/corejava/assignment01/Problem_02.java
+++ JavaAssignment/src/corejava/assignment01/Problem_02.java
@@ -4,11 +4,19 @@ package corejava.assignment01;
  * Its a division Problem .
  */
 public class Problem_02 {
-	public static void main(String[] args) {
-		System.out.println("9.5 X 4.5 - 2.5 X 3");
-		System.out.println("-------------------");
-		System.out.println("     45.5 - 3.5");
-		System.out.println("         =");
+
+		public static void main(String[] args) {
+
+
+		double x = 9.5 * 4.5 ;
+		double y = 2.5 * 3 ;
+		double z = x - y;
+			System.out.println("9.5 X 4.5 - 2.5 X 3=\t " + z);
+		    System.out.println("----------------------------------");
+		double a = 45.5 - 3.5;
+		    System.out.println(" 45.5 - 3.5=\t" + a);
+		double answer = z / a;
+		    System.out.println("  Answer   =" +"\t" + answer);
 
 	}
 }
