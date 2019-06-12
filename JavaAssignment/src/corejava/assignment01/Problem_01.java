f75b1837e0eedf801be69dabee4ce33d32099af2
lomeshnaik <lomeshnaik@yahoo.com>
2019-06-11 23:06:57 -0400

    Declaring Class Variables......(must include static and declare in class body)";

--- JavaAssignment/src/corejava/assignment01/Problem_01.java
+++ JavaAssignment/src/corejava/assignment01/Problem_01.java
@@ -4,8 +4,14 @@ package corejava.assignment01;
  * Print Hello World as the output of this program Two times.
  * I have printed only one time.
  */
+
 public class Problem_01 {
+
+    static String helloMessage;// Declaring Class Variables......(must include static and declare in class body)
     public static void main(String[] args) {
+
+        helloMessage = " I Solved Problem 1: and  Declaring Class Variables......(must include static and declare in class body)";
         System.out.println("Hello World !!!");
+        System.out.println(helloMessage);
     }
 }
