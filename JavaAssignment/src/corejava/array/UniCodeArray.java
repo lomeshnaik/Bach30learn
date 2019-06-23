686d6f6c3fa7459184f49c1b801a98103f59b18d
jayramrout <jayram.h2kinfosys@gmail.com>
2019-06-23 16:13:12 -0400

    update

--- JavaAssignment/src/corejava/array/UniCodeArray.java
+++ JavaAssignment/src/corejava/array/UniCodeArray.java
@@ -35,4 +35,18 @@ Enter the End value in Unicode Hex:
 	!	"	#	$	%	&	'	(	)	*	+	,	-	.	/	0	1	2	3	4	5
 	6	7	8	9	:	;	<	=	>	?	@	A	B	C
 	D	E	F	G	H	I	J	K	L	M	N	O	P	Q	R	S	T	U	V	W	X	Y	Z
+
+	Enter the Start value in Unicode Hex:
+0900
+2304 ऀ
+Enter the End value in Unicode Hex:
+094c
+2380 ौ
+76 characters
+	ऀ	ँ	ं	ः	ऄ	अ	आ	इ	ई	उ	ऊ	ऋ	ऌ	ऍ	ऎ	ए
+	ऐ	ऑ	ऒ	ओ	औ	क	ख	ग	घ	ङ	च	छ	ज	झ	ञ	ट	ठ	ड
+	ढ	ण	त	थ	द	ध	न	ऩ	प	फ	ब	भ	म	य
+	र	ऱ	ल	ळ	ऴ	व	श	ष	स	ह	ऺ	ऻ	़	ऽ	ा	ि	ी	ु	ू	ृ	ॄ	ॅ
+	ॆ	े	ै	ॉ	ॊ	ो	ौ
+Process finished with exit code 0
  */
